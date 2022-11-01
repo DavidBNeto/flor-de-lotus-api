9c0378fe0d546f6f326b2d75c2796b4e7822f067
MiguelZanela <57719624+MiguelZanela@users.noreply.github.com>
2022-11-01 17:29:48 -0300

    [Fix] Code smells

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,4 @@
-import {Get, Controller, Post, Body, Res, Param, Put, Delete} from '@nestjs/common';
+import {Get, Controller, Post, Body, Res, Delete} from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserModel } from './models/user.model';
 import { typesRole } from './schemas/user.schema';
